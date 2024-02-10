cee9d3591f6f3f05ff06b987f25d2ec2ac545c12
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-11 01:24:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const nav = document.querySelector('nav');
 
 let list = ["Home","About","Contact","Itinerary"];
 let ul = document.createElement('ul');
-let li =document.createElement('li');
-list.forEach((item)=>{
-	li.innerText += item;
-	
-})
-ul.appendChild(li);
 
+list.forEach((item)=>{
+	let li =document.createElement('li');
+	li.innerText = item;
+	ul.appendChild(li);
+})
 nav.appendChild(ul);
 
