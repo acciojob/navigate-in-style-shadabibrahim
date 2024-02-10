f6da1eb509299c1f70c96c23a3bb901224f51f8e
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-11 00:56:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //your JS code here. If required.
+const nav = document.querySelector('nav');
+
 let list = ["Home","About","Contact","Itinerary"];
-let ul = doucument.createElement('ul');
-let li = doucument.createElement('li');
+let ul = document.createElement('ul');
+let li =document.createElement('li');
+list.forEach((item)=>{
+	li.innerText += item;
+	
+})
+ul.appendChild(li);
+ul.setAttribute("className","container  li")
+nav.appendChild(ul);
+
